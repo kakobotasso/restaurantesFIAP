90d357ca084a7b06e196f461bed851176394e12f
Kako Botasso <kakodev@gmail.com>
2017-03-04 20:33:53 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/kakobotasso/restaurantesfiap/SplashSreenActivity.java
@@ -0,0 +1,13 @@
+package br.com.kakobotasso.restaurantesfiap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashSreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_sreen);
+    }
+}
